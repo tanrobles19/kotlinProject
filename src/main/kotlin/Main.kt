36f9be64f3c84212b220039b686fbcbb9e639b0b
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-07-29 14:10:00 -0600

    testing

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+
+// A compile-time constant must be defined outside any function
+const val HERO_NAME = "Madrigal"
+
+fun main() {
+
+    println(HERO_NAME)
+
+    var playerLevel = 4
+    playerLevel+=4
+    println(playerLevel)
+}
