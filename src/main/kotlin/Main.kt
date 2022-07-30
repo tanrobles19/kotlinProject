fa985b8851996eb53096a900d762b29589360b1e
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-07-30 11:47:17 -0600

    Range Operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,17 @@ const val HERO_NAME = "Madrigal"
 
 fun main() {
 
-    println(HERO_NAME)
+    println(HERO_NAME.reversed())
 
-    var playerLevel = 4
-    playerLevel+=4
+    var playerLevel = 0
+//    playerLevel+=4
     println(playerLevel)
+
+    test(playerLevel)
+
+}
+
+private fun test(playerLevel : Int) {
+    val message = if( playerLevel in 1..6) "Eithan" else "Issac"
+    println(message)
 }
