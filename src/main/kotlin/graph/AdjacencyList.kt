300f70236fb3cf95246ddf303938cfc5f572e2b0
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-08-09 11:26:08 -0600

    Graph implementation
    Based on Adjacency List impl

--- src/main/kotlin/graph/AdjacencyList.kt
+++ src/main/kotlin/graph/AdjacencyList.kt
@@ -11,23 +11,37 @@ class AdjacencyList<T: Any> : Graph<T> {
     }
 
     override fun addDirectedEdge(source: Vertex<T>, destination: Vertex<T>, weight: Double?) {
-        TODO("Not yet implemented")
+        val edge = Edge(source, destination, weight)
+        adjacencyList[source]?.add(edge)
     }
 
     override fun addUnDirectedEdge(source: Vertex<T>, destination: Vertex<T>, weight: Double?) {
-        TODO("Not yet implemented")
+        addDirectedEdge(source, destination, weight)
+        addDirectedEdge(destination, source, weight)
     }
 
     override fun add(edge: EdgeType, source: Vertex<T>, destination: Vertex<T>, weight: Double?) {
-        TODO("Not yet implemented")
+        when(edge) {
+            EdgeType.DIRECTED -> addDirectedEdge(source, destination, weight)
+            EdgeType.UNDIRECTED -> addUnDirectedEdge(source, destination, weight)
+        }
     }
 
-    override fun edges(source: Vertex<T>): ArrayList<Edge<T>> {
-        TODO("Not yet implemented")
-    }
+    override fun edges(source: Vertex<T>) = adjacencyList[source]?: arrayListOf()
 
+    // Retrieving the weight of an edge
     override fun weight(source: Vertex<T>, destination: Vertex<T>): Double? {
-        TODO("Not yet implemented")
+        return edges(source).firstOrNull{ it.destination == destination }?.weight
+    }
+
+    override fun toString(): String {
+        return buildString {
+            adjacencyList.forEach{vertex, edge ->
+                val edgeString = edge.joinToString { it.destination.data.toString() }
+                append("${vertex.data} --> [ $edgeString ] \n")
+
+            }
+        }
     }
 
 }
