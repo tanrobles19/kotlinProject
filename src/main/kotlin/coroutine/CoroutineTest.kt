d6b6f0e8bcfe12b33bfad1b5a65b331371c54f68
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-08-11 17:43:05 -0600

    Coroutines Sequential Implementation

--- src/main/kotlin/coroutine/CoroutineTest.kt
+++ src/main/kotlin/coroutine/CoroutineTest.kt
@@ -1,3 +1,5 @@
+import kotlinx.coroutines.*
+import java.lang.Math.log
 import java.net.URL
 
 private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
@@ -5,10 +7,50 @@ private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
 
 fun main() {
 
-    println("Start fetching process...")
-    println(fetchFlight())
-    println("End fetching process...")
 
 }
 
-fun fetchFlight(): String = URL(FLIGHT_ENDPOINT).readText()
+
+//Sequential Implementation
+fun sequentialNetworkRequest() {
+
+    println("1")
+
+    runBlocking {
+
+        println("Started")
+
+        launch {
+            // Sequential network request
+            println("call flight")
+            println("Flight Info" + fetchFlight(FLIGHT_ENDPOINT))
+
+            println("call loyalty")
+            println("Loyalty" + fetchFlight("http://kotlin-book.bignerdranch.com/2e/loyalty"))
+        }
+
+        println("End")
+
+    }
+
+    println("2")
+
+}// end fun sequentialNetworkRequest()
+
+fun unManagedThread() {
+    GlobalScope.launch {
+        println("I am GlobalCoroutine!")
+    }
+
+// you need to execute another task to show the GlobalScope
+    (1..1000).forEach {
+        println(it)
+    }
+}
+
+suspend fun fetchFlight(url: String): String {
+    return withContext(Dispatchers.IO) {
+        URL(url).readText()
+    }
+}
+
