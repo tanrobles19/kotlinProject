d25f1a028ae944c118ea3899a550d3e02fa4b4e0
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-08-14 16:42:03 -0600

    Sum Two numbers
    Using a Hash Table data structure we can to improve the algorithm. Now is a O(n2) time complexity.

--- src/main/kotlin/algorithms/TwoSumChallenge.kt
+++ src/main/kotlin/algorithms/TwoSumChallenge.kt
@@ -2,21 +2,45 @@ fun main( ) {
 
     val nums : IntArray = intArrayOf(2,7,11,15)
 
-    println(sumTwo2(nums, 9).toList())
+    println(sumTwo(nums, 9).toList())
 
 }
+// Big O(n2) time complexity(Linear time)
+fun sumTwo(nums: IntArray, target: Int): IntArray {
 
+    val hash : HashMap<Int, Int> = hashMapOf()
+
+    for (i in nums.indices) {
+
+        val hashNumber = hash[target - nums[i]]
+
+        if(hashNumber != null) return intArrayOf(hashNumber, i) else hash[nums[i]] = i
+
+    }// end for
+
+    return IntArray(2)
+
+}// end fun sumTwo
+
+//Big O(n2) Worst solution(two for loop)
 fun sumTwo2(nums: IntArray, target: Int): IntArray {
 
     val index : IntArray = IntArray(2)
 
     for (i in nums.indices) {
+
+        if(nums[i] > target) continue
+
         for (x in nums.indices) {
-            if( i != x && (nums[i] + nums[x]) == target ) {
+
+            if((nums[i] + nums[x + i + 1]) == target ) {
                 index[0] = i
-                index[1] = x
+                index[1] = x + i + 1
                 return index
             }// end if
+
+            if((x + i + 2) >= nums.size)
+                break
         }// end for
     }// end for
 
